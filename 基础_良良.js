7e8c0e78e9930451c4505053d89e43f1ed44d2ad
ffyuhf <101410685+ffyuhf@users.noreply.github.com>
2024-11-10 04:21:12 +0800

    更新基本UA
    更新了标准UA,短UA,TBS(腾讯移动服务UA),iPoneUA(可能有问题)

--- 基础_良良.js
+++ 基础_良良.js
@@ -1,11 +1,11 @@
 加入("基础",{
 	std: {
 		名称: "标准",
-		内容: `Mozilla/5.0 (Linux; Android 12.0.1; zh-cn; Pixel 6 Pro; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/98.0.4758.46 Mobile Safari/537.36`
+		内容: `Mozilla/5.0 (Linux; Android 14; zh-cn; Pixel 6 Pro; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.73 Mobile Safari/537.36`
 	},
 	short: {
 		名称: "短 UA",
-		内容: `Mozilla/5.0 (Linux; U; Android 12.0.1; zh-cn; Pixel 6 Pro)`
+		内容: `Mozilla/5.0 (Linux; U; Android 14; zh-cn; Pixel 6 Pro)`
 	},
 	wp: {
 		名称: "WP",
@@ -13,7 +13,7 @@
 	},
 	tbs: {
 		名称: "TBS",
-		内容: `Mozilla/5.0 (Linux; Android 12.0.1; zh-cn; Pixel 6 Pro; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045912 Mobile Safari/537.36`
+		内容: `Mozilla/5.0 (Linux; Android 14; zh-cn; Pixel 6 Pro; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/121.0.6167.71 MQQBrowser/6.2 TBS/047205 Mobile Safari/537.36`
 	},
 	symb: {
 		名称: "塞班",
@@ -21,7 +21,7 @@
 	},
 	ios: {
 		名称: "iPhone",
-		内容: "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1"
+		内容: "Mozilla/5.0 (iPhone; CPU iPhone OS 18_1 like Mac OS X) AppleWebKit/605.4.6 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1"
 	},
 	blank: {
 		名称: "空白",
